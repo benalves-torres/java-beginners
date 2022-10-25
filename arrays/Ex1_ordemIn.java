118b0cc684ab36b4e988fb4d78f4db10e7c29959
benalves-torres <ben.alves@ifood.com.br>
2022-10-25 20:58:45 -0300

    Exercício Ordem Inversa

---
+++ arrays/Ex1_ordemIn.java
@@ -0,0 +1,22 @@
+public class Ex1_ordemIn {
+    
+    public static void main(String[] args) {
+        
+        int[] vetor = {0, -5, 15, 50, 8, 4};
+
+        //System.out.println(vetor.length);
+
+        System.out.println("Vetor: ");
+        int count = 0;
+        while(count < (vetor.length-1)) {
+            System.out.println(vetor[count] + " ");
+            count++;
+    }
+
+    System.out.println("\nVetor: ");
+    for(int i = (vetor.length-1); i >= 0; i --) {
+        System.out.println(vetor[i] + " ");
+
+    }
+}
+}
