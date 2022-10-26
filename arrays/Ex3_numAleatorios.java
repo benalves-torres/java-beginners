39d88ffd60d1ffa47be2d2d5b46f2e91cf481cf9
benalves-torres <ben.alves@ifood.com.br>
2022-10-25 21:21:30 -0300

    Exercício

--- arrays/Ex3_numAleatorios.java
+++ arrays/Ex3_numAleatorios.java
@@ -1,3 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex3_numAleatorios {
     
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i <numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+
+        }
+
+        System.out.println("Números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Sucessores dos números Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero+1) + " ");
+        }
+    }
 }
